09b70f4c4de9e4a14987763dc4e368b80d477a8b
Rahmonjon22 <rahmonjon2@yahoo.com>
2022-07-01 21:05:34 +0200

    learning

---
+++ app.js
@@ -0,0 +1,23 @@
+//console.log('My code is in here ...');
+// ** Javascript adding **
+// function add(n1, n2){
+//     return n1 + n2;
+// }
+// const number1 = 5;
+// const number2 = 2.8;
+// const result = add(number1, number2);
+// console.log(result);
+// function add(n1, n2){
+//     return n1 + n2;
+// }
+// const number1 = '5';
+// const number2 = 2.8;
+// const result = add(number1, number2);
+// console.log(result);
+function add(n1, n2) {
+    return n1 + n2;
+}
+var number1 = '5';
+var number2 = 2.8;
+var result = add(number1, number2);
+console.log(result);
---
+++ app.ts
@@ -0,0 +1,33 @@
+//console.log('My code is in here ...');
+
+
+// ** Javascript adding **
+// function add(n1, n2){
+//     return n1 + n2;
+// }
+
+// const number1 = 5;
+// const number2 = 2.8;
+
+// const result = add(number1, number2);
+// console.log(result);
+
+// function add(n1, n2){
+//     return n1 + n2;
+// }
+
+// const number1 = '5';
+// const number2 = 2.8;
+
+// const result = add(number1, number2);
+// console.log(result);
+
+ function add(n1: number, n2: number){
+    return n1 + n2;
+}
+
+const number1 = '5';
+const number2 = 2.8;
+
+const result = add(number1, number2);
+console.log(result);
