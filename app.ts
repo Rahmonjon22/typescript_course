efa0244fc82c15592819f12edc30df7a9342c302
Rahmonjon22 <rahmonjon2@yahoo.com>
2022-07-01 22:29:02 +0200

    weitere

--- app.ts
+++ app.ts
@@ -22,12 +22,44 @@
 // const result = add(number1, number2);
 // console.log(result);
 
- function add(n1: number, n2: number){
-    return n1 + n2;
+//  function add(n1: number, n2: number){
+//     // console.log(typeof number1); this is to check whether it is string or number
+//     // if (typeof n1 !== 'number' || typeof number2 !== 'number') {
+//     //     throw new Error('incorrect input!');
+//     // }
+//     // this  above is JS way of doing it  
+//     return n1 + n2;
+// }
+
+// const number1 = 5; // 5.0 the same
+// const number2 = 2.8;
+
+// const result = add(number1, number2);
+// console.log(result);
+
+
+
+function add(n1: number, n2: number, showResult: boolean, phrase: string){
+    // console.log(typeof number1); this is to check whether it is string or number
+    // if (typeof n1 !== 'number' || typeof number2 !== 'number') {
+    //     throw new Error('incorrect input!');
+    // }
+    // this  above is JS way of doing it  
+    if(showResult){
+        console.log(phrase + n1 + n2);
+    } else {
+        return n1 + n2;
+    }
+    
 }
 
-const number1 = '5';
+const number1 = 5; // 5.0 the same
 const number2 = 2.8;
+const printResult = true;
+const resultPhrase = 'Result is: ';
 
-const result = add(number1, number2);
-console.log(result);
+add(number1, number2, printResult, resultPhrase);
+
+
+
+
