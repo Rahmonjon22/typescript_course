fe0e9e51f73b28fa3ca38052b01bb459c93d522b
Rahmonjon22 <rahmonjon2@yahoo.com>
2022-07-03 13:38:06 +0200

    weitere lern

--- app.js
+++ app.js
@@ -1,46 +1,6 @@
-//console.log('My code is in here ...');
-// ** Javascript adding **
-// function add(n1, n2){
-//     return n1 + n2;
-// }
-// const number1 = 5;
-// const number2 = 2.8;
-// const result = add(number1, number2);
-// console.log(result);
-// function add(n1, n2){
-//     return n1 + n2;
-// }
-// const number1 = '5';
-// const number2 = 2.8;
-// const result = add(number1, number2);
-// console.log(result);
-//  function add(n1: number, n2: number){
-//     // console.log(typeof number1); this is to check whether it is string or number
-//     // if (typeof n1 !== 'number' || typeof number2 !== 'number') {
-//     //     throw new Error('incorrect input!');
-//     // }
-//     // this  above is JS way of doing it  
-//     return n1 + n2;
-// }
-// const number1 = 5; // 5.0 the same
-// const number2 = 2.8;
-// const result = add(number1, number2);
-// console.log(result);
-function add(n1, n2, showResult, phrase) {
-    // console.log(typeof number1); this is to check whether it is string or number
-    // if (typeof n1 !== 'number' || typeof number2 !== 'number') {
-    //     throw new Error('incorrect input!');
-    // }
-    // this  above is JS way of doing it  
-    if (showResult) {
-        console.log(phrase + n1 + n2);
-    }
-    else {
-        return n1 + n2;
-    }
-}
-var number1 = 5; // 5.0 the same
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = 'Result is: ';
-add(number1, number2, printResult, resultPhrase);
+var person = {
+    name: 'Rahmonjon',
+    age: 30,
+    nickname: 'Jon'
+};
+console.log(person.nickname);
