2c1f6d29e66950bd7e917d1cfcd79968a7548f85
Rahmonjon22 <rahmonjon2@yahoo.com>
2022-07-03 13:49:45 +0200

    weiteire

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
+// how typescript can make everything more accurate by signing each object 
+//** Objects in Typescript */
+// const person: {
+//     name: string;
+//     age: number;
+//     nickname: string;
+// } = {
+//     name: 'Rahmonjon',
+//     age: 30,
+//     nickname: 'Jon'
+// };
+// console.log(person.nickname);
+//** Arrays in Typescript */
 var person = {
     name: 'Rahmonjon',
     age: 30,
-    nickname: 'Jon'
+    nickname: 'Jon',
+    hobbies: ['Sports', 'Cooking']
 };
-console.log(person.nickname);
+// let favouriteActivities: any[];
+var favouriteActivities;
+favouriteActivities = ['Sports'];
+console.log(person.hobbies);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+    // console.log(hobby.map()); it works with arrays in Javascript
+}
--- app.ts
+++ app.ts
@@ -1,7 +1,34 @@
+
+// how typescript can make everything more accurate by signing each object 
+//** Objects in Typescript */
+// const person: {
+//     name: string;
+//     age: number;
+//     nickname: string;
+// } = {
+//     name: 'Rahmonjon',
+//     age: 30,
+//     nickname: 'Jon'
+// };
+
+// console.log(person.nickname);
+
+//** Arrays in Typescript */
+
 const person = {
     name: 'Rahmonjon',
     age: 30,
-    nickname: 'Jon'
+    nickname: 'Jon',
+    hobbies: ['Sports', 'Cooking']
 };
+// let favouriteActivities: any[];
 
-console.log(person.nickname);
+let favouriteActivities: string[];
+favouriteActivities= ['Sports'];
+console.log(person.hobbies);
+
+
+for( const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+   // console.log(hobby.map()); it works with arrays in Javascript
+}
