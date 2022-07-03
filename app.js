ddb92292086194290a6ed4f62a0883ecae43dc60
Rahmonjon22 <rahmonjon2@yahoo.com>
2022-07-03 22:09:23 +0200

    Union & Literal types

--- app.js
+++ app.js
@@ -1,85 +1,15 @@
-// how typescript can make everything more accurate by signing each object 
-//** Objects in Typescript */
-// const person: {
-//     name: string;
-//     age: number;
-//     nickname: string;
-// } = {
-//     name: 'Rahmonjon',
-//     age: 30,
-//     nickname: 'Jon'
-// };
-// console.log(person.nickname);
-//** Arrays in Typescript */
-// const person = {
-//     name: 'Rahmonjon',
-//     age: 30,
-//     nickname: 'Jon',
-//     hobbies: ['Sports', 'Cooking']
-// };
-// // let favouriteActivities: any[];
-// let favouriteActivities: string[];
-// favouriteActivities= ['Sports'];
-// console.log(person.hobbies);
-// for( const hobby of person.hobbies) {
-//     console.log(hobby.toUpperCase());
-//    // console.log(hobby.map()); it works with arrays in Javascript
-// }
-//** Tuple in Typescript */
-// fixed length
-// const person: {
-//     name: string;
-//     age: number;
-//     nickname: string;
-//     hobbies: string[];
-//     role: [number, string];
-// } = {
-//     name: 'Rahmonjon',
-//     age: 30,
-//     nickname: 'Jon',
-//     hobbies: ['Sports', 'Cooking'], // array with typescript
-//     role: [2, 'author'] // array with tuple, first element is numeric and second element is string
-// };
-// person.role.push('admin');
-// // person.role[1] = 10;
-// // person.role = [0, 'admin', 'user']
-// let favouriteActivities: string[];
-// favouriteActivities= ['Sports'];
-// console.log(person.role);
-//** Enum in Typescript */
-// const ADMIN = 0; // strings
-// const READ_ONLY = 1;
-// const AUTHER = 2;
-// const person = {
-//     name: 'Rahmonjon',
-//     age: 30,
-//     nickname: 'Jon',
-//     hobbies: ['Sports', 'Cooking'],
-//     role: ADMIN
-// };
-// if (person.role === ADMIN) {
-//     console.log('is admin')
-// }
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHER"] = 2] = "AUTHER";
-})(Role || (Role = {}));
-;
-var person = {
-    name: 'Rahmonjon',
-    age: 30,
-    hobbies: ['Sports', 'Cooking'],
-    role: Role.ADMIN
-};
-var favouriteActivities;
-favouriteActivities = ['Sports'];
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase);
+//** Union Types in typescript */
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
-if (person.role === Role.AUTHER) {
-    console.log('is auther');
-}
+var combineAges = combine(30, 26);
+console.log(combineAges);
+var combineNames = combine('Max', 'Anna');
+console.log(combineNames);
--- basic.ts
+++ basic.ts
@@ -39,26 +39,26 @@
 
 
 
-function add(n1: number, n2: number, showResult: boolean, phrase: string){
-    // console.log(typeof number1); this is to check whether it is string or number
-    // if (typeof n1 !== 'number' || typeof number2 !== 'number') {
-    //     throw new Error('incorrect input!');
-    // }
-    // this  above is JS way of doing it  
-    if(showResult){
-        console.log(phrase + n1 + n2);
-    } else {
-        return n1 + n2;
-    }
+// function add(n1: number, n2: number, showResult: boolean, phrase: string){
+//     // console.log(typeof number1); this is to check whether it is string or number
+//     // if (typeof n1 !== 'number' || typeof number2 !== 'number') {
+//     //     throw new Error('incorrect input!');
+//     // }
+//     // this  above is JS way of doing it  
+//     if(showResult){
+//         console.log(phrase + n1 + n2);
+//     } else {
+//         return n1 + n2;
+//     }
     
-}
+// }
 
-const number1 = 5; // 5.0 the same
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result is: ';
+// const number1 = 5; // 5.0 the same
+// const number2 = 2.8;
+// const printResult = true;
+// const resultPhrase = 'Result is: ';
 
-add(number1, number2, printResult, resultPhrase);
+// add(number1, number2, printResult, resultPhrase);
 
 
 
