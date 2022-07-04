ee38069e419b7bab542cf95715144dd5df5168f9
Rahmonjon22 <rahmonjon2@yahoo.com>
2022-07-04 17:31:46 +0200

    callback & functions

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
-//** Union Types in typescript */
-function combine(input1, input2, resultConversion // instead of using string 
-) {
-    var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-        result = +input1 + +input2; // for each converted into number
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
-    // if (resultConversion === 'as-number') {
-    //      return +result;
-    // } else {
-    //     return result.toString();
-    // }
+//** Functions return Types && Void */
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combineAges = combine(30, 26, 'as-number');
-console.log(combineAges);
-var combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedStringAges);
-var combineNames = combine('Max', 'Anna', 'as-text');
-console.log(combineNames);
+//  //** Void function, it does not necessarly be there  */
+// function printResult(num: number): void {
+//     console.log('Result: ' + num);
+// }
+// printResult(add(5, 12));
+// console.log(printResult(add(5, 12)));
+//** underfined function, it does not necessarly be there  */
+//  function printResult(num: number): undefined {
+//     console.log('Result: ' + num);
+//     return;
+// }
+// printResult(add(5, 12));
+// Function Types allows us to use specific which type of function wheter it is parameter or callbacks
+function printResult(num) {
+    console.log('Result: ' + num);
+}
+function addAndHandle(n1, n2, callback) {
+    var result = n1 + n2;
+    callback(result);
+}
+printResult(add(5, 12));
+var combineValues;
+// let combineValues: Function;
+combineValues = add;
+// combineValues= printResult; function has to be function types so that we dont run it problem
+// combineValues = 6; that is only caught in browser. therefore it has to be function
+console.log(combineValues(8, 8));
+addAndHandle(10, 20, function (result) {
+    console.log(result); // callback here and typescript knows it is number
+    return result;
+}); // expected result : 30 
--- app.ts
+++ app.ts
@@ -1,79 +1,44 @@
-//** Union Types in typescript */
+//** Functions return Types && Void */
 
-// function combine(input1: number | string, input2: number | string,) {
-//     let result;
-//     if (typeof input1 === 'number' && typeof input2 === 'number') {
-//         result = input1 + input2;
-//     } else {
-//         result = input1.toString() + input2.toString();
-//     }
-//     return result;
-// }
-// const combineAges = combine(30, 26);
-
-// console.log(combineAges);
-
-// const combineNames = combine('Max', 'Anna');
-// console.log(combineNames);
-
-
-//** Literal types in TypeScript */
-
-// function combine(
-//     input1: number | string, 
-//     input2: number | string,
-//     resultConversion : 'as-number' | 'as-text' // instead of using string 
-//     ) {
-//     let result;
-//     if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-//         result = +input1 + +input2; // for each converted into number
-//     } else {
-//         result = input1.toString() + input2.toString();
-//     }
-//     return result;
-//     // if (resultConversion === 'as-number') {
-//     //      return +result;
-//     // } else {
-//     //     return result.toString();
-//     // }
-   
-// }
-// const combineAges = combine(30, 26, 'as-number');
-// console.log(combineAges);
-// const combinedStringAges = combine('30', '26', 'as-number');
-// console.log(combinedStringAges);
-
-// const combineNames = combine('Max', 'Anna', 'as-text');
-// console.log(combineNames);
-
-
-//**  types Aliases in TypeScript */
-
-type Combinable = number | string;
-type ConversionDescriptor = 'as-number' | 'as-text';
-function combine(
-    input1: Combinable, 
-    input2: Combinable,
-    resultConversion : ConversionDescriptor // instead of using string 
-    ) {
-    let result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-        result = +input1 + +input2; // for each converted into number
-    } else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
-    // if (resultConversion === 'as-number') {
-    //      return +result;
-    // } else {
-    //     return result.toString();
-    // }
-   
+function add(n1: number, n2: number): number{
+    return n1 + n2;
 }
-const combineAges = combine(30, 26, 'as-number');
-console.log(combineAges);
-const combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedStringAges);
+//  //** Void function, it does not necessarly be there  */
+// function printResult(num: number): void {
+//     console.log('Result: ' + num);
+// }
+// printResult(add(5, 12));
+// console.log(printResult(add(5, 12)));
 
-const combineNames = combine('Max', 'Anna', 'as-text');
-console.log(combineNames);
+ //** underfined function, it does not necessarly be there  */
+//  function printResult(num: number): undefined {
+//     console.log('Result: ' + num);
+//     return;
+// }
+// printResult(add(5, 12));
+
+// Function Types allows us to use specific which type of function wheter it is parameter or callbacks
+
+function printResult(num: number): void {
+    console.log('Result: ' + num);
+}
+function addAndHandle(n1: number, n2: number, callback: (num: number) => void){
+    const result = n1 + n2;
+    callback(result); 
+}
+
+printResult(add(5, 12));
+
+let combineValues: (a: number, b: number) => number;
+// let combineValues: Function;
+combineValues = add;
+
+// combineValues= printResult; function has to be function types so that we dont run it problem
+// combineValues = 6; that is only caught in browser. therefore it has to be function
+
+console.log(combineValues(8, 8));
+
+addAndHandle(10, 20, (result)=> {
+    console.log(result); // callback here and typescript knows it is number
+    return result;
+})// expected result : 30 
