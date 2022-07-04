8164824a62a568fff6144ffa6a37be30331713a1
Rahmonjon22 <rahmonjon2@yahoo.com>
2022-07-04 20:11:41 +0200

    weitere

--- app.ts
+++ app.ts
@@ -6,14 +6,15 @@ let userName: string;
 
 userInput = 5;
 userInput = 'Max';
-if(typeof userInput === 'string') {
+if (typeof userInput === 'string') {
     userName = userInput;
 }
 
 
 //** Never in TypeScript */
-function generateError(message: string, code: number){
+function generateError(message: string, code: number): never {
     throw {message: message, errorCode: code}
+   // while (true) {}
 }
 const result = generateError('An error occured!', 500);
 console.log(result);
